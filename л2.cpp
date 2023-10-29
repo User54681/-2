90108768b8ad7b9498d30507e0d2c3d6d3913c11
User54681 <anzolotylh@gmail.com>
2023-10-29 15:11:25 +0300

    пункт 1

--- л2.cpp
+++ л2.cpp
@@ -1,7 +1,42 @@
 ﻿#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::cin;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
+    int var, size;
+    std::vector<std::string> students1{ "Иван", "Анастасия", "Анна", "Екатерина", "Михаил", "Сергей", "Александр", "Татьяна", "Валерий", "Полина" },
+                             students2(size),
+                             students3(size);
+    std::vector<int> marks1{74, 83, 99, 50, 80, 90, 70, 84, 100, 74}, marks2(size), marks3(size);
+
+    cout << "Введите:" << "\n" << "1. Если хотите демонстрационный анализ на дефолтных входных значениях" << "\n"
+         << "2. Если хотите анализ на случайных входных значениях" << "\n"
+         << "3. Если хотите анализ на входных значениях от самого пользователя" << "\n";
+    cin >> var;
+
+    switch (var) {
+        
+        case 1: 
+        {
+            cout << "Имя" << "\t" << "Балл" << "\n";
+            for (int i = 0; i < 10; ++i) cout << students1[i] << "\t" << marks1[i] << "\n";
+        }
+        case 2:
+        {
+            cout << "";
+            cin >> size;
+
+        }
+        case 3:
+        {
+            cout << "";
+            cin >> size;
+        }
+    }
+
 }
 
